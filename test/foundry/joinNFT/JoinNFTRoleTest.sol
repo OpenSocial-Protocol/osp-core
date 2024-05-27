3d4f9eaa355736f3daac8de88dc43e6d4fe6792b
XuZhanXin <zhanxin.xu@kikitrade.com>
2024-05-27 17:28:25 +0800

    feat: add revoke role test

--- test/foundry/joinNFT/JoinNFTRoleTest.sol
+++ test/foundry/joinNFT/JoinNFTRoleTest.sol
@@ -63,6 +63,64 @@ contract JoinNFTRoleTest is JoinNFTTestSetUp {
         joinNFT.setAdmin(member, true);
     }
 
+    function test_RevokeAdminRole_Owner() public {
+        assert(joinNFT.hasOneRole(Constants.COMMUNITY_ADMIN_ACCESS, admin));
+        assert(joinNFT.hasAllRole(Constants.COMMUNITY_ADMIN_ACCESS, admin));
+        assert(
+            ospClient.hasOneCommunityRole(
+                TEST_COMMUNITY_ID,
+                Constants.COMMUNITY_ADMIN_ACCESS,
+                admin
+            )
+        );
+        assert(
+            ospClient.hasAllCommunityRole(
+                TEST_COMMUNITY_ID,
+                Constants.COMMUNITY_ADMIN_ACCESS,
+                admin
+            )
+        );
+        vm.expectEmit(address(ospClient));
+        emit OspEvents.JoinNFTRoleChanged(
+            TEST_COMMUNITY_ID,
+            owner,
+            admin,
+            Constants.COMMUNITY_ADMIN_ACCESS,
+            false,
+            block.timestamp
+        );
+        vm.prank(owner);
+        assert(joinNFT.setAdmin(admin, false));
+        assertFalse(joinNFT.hasOneRole(Constants.COMMUNITY_ADMIN_ACCESS, admin));
+        assertFalse(joinNFT.hasAllRole(Constants.COMMUNITY_ADMIN_ACCESS, admin));
+        assertFalse(
+            ospClient.hasOneCommunityRole(
+                TEST_COMMUNITY_ID,
+                Constants.COMMUNITY_ADMIN_ACCESS,
+                admin
+            )
+        );
+        assertFalse(
+            ospClient.hasAllCommunityRole(
+                TEST_COMMUNITY_ID,
+                Constants.COMMUNITY_ADMIN_ACCESS,
+                admin
+            )
+        );
+    }
+
+    function testRevert_RevokeAdminRole_NotOwner() public {
+        vm.expectRevert(OspErrors.NotCommunityOwner.selector);
+        vm.prank(admin);
+        joinNFT.setAdmin(admin, false);
+        vm.expectRevert(OspErrors.NotCommunityOwner.selector);
+        vm.prank(mod);
+        joinNFT.setAdmin(admin, false);
+        vm.expectRevert(OspErrors.NotCommunityOwner.selector);
+        vm.prank(member);
+        joinNFT.setAdmin(admin, false);
+    }
+
     // mod test
     function test_GrantModRole_Owner() public {
         _grantModRole(owner);
@@ -127,6 +185,69 @@ contract JoinNFTRoleTest is JoinNFTTestSetUp {
         joinNFT.setModerator(member, true);
     }
 
+    function test_RevokeModRole_Owner() public {
+        _revokeModRole(owner);
+    }
+
+    function test_RevokeModRole_Admin() public {
+        _revokeModRole(admin);
+    }
+
+    function _revokeModRole(address sender) internal {
+        assert(joinNFT.hasOneRole(Constants.COMMUNITY_MODERATOR_ACCESS, mod));
+        assert(joinNFT.hasAllRole(Constants.COMMUNITY_MODERATOR_ACCESS, mod));
+        assert(
+            ospClient.hasOneCommunityRole(
+                TEST_COMMUNITY_ID,
+                Constants.COMMUNITY_MODERATOR_ACCESS,
+                mod
+            )
+        );
+        assert(
+            ospClient.hasAllCommunityRole(
+                TEST_COMMUNITY_ID,
+                Constants.COMMUNITY_MODERATOR_ACCESS,
+                mod
+            )
+        );
+        vm.expectEmit(address(ospClient));
+        emit OspEvents.JoinNFTRoleChanged(
+            TEST_COMMUNITY_ID,
+            sender,
+            mod,
+            Constants.COMMUNITY_MODERATOR_ACCESS,
+            false,
+            block.timestamp
+        );
+        vm.prank(sender);
+        assert(joinNFT.setModerator(mod, false));
+        assertFalse(joinNFT.hasOneRole(Constants.COMMUNITY_MODERATOR_ACCESS, mod));
+        assertFalse(joinNFT.hasAllRole(Constants.COMMUNITY_MODERATOR_ACCESS, mod));
+        assertFalse(
+            ospClient.hasOneCommunityRole(
+                TEST_COMMUNITY_ID,
+                Constants.COMMUNITY_MODERATOR_ACCESS,
+                mod
+            )
+        );
+        assertFalse(
+            ospClient.hasAllCommunityRole(
+                TEST_COMMUNITY_ID,
+                Constants.COMMUNITY_MODERATOR_ACCESS,
+                mod
+            )
+        );
+    }
+
+    function testRevert_RevokeModRole_NotOwnerOrAdmin() public {
+        vm.expectRevert(OspErrors.JoinNFTUnauthorizedAccount.selector);
+        vm.prank(mod);
+        joinNFT.setModerator(mod, false);
+        vm.expectRevert(OspErrors.JoinNFTUnauthorizedAccount.selector);
+        vm.prank(member);
+        joinNFT.setModerator(mod, false);
+    }
+
     // member level test
     function test_SetMemberLevel_Owner() public {
         _setMemberLevel(owner);
@@ -189,36 +310,49 @@ contract JoinNFTRoleTest is JoinNFTTestSetUp {
             block.timestamp
         );
         vm.prank(sender);
-        joinNFT.setBlockList(member, true);
+        assert(joinNFT.setBlockList(member, true));
         assert(joinNFT.isBlock(member));
         assert(ospClient.isCommunityBlock(TEST_COMMUNITY_ID, member));
     }
 
-    function testRevert_BlockAccount_NotOwnerOrAdminOrMod() public {
+    function test_UnblockAccount_Owner() public {
+        _unblockAccount(owner);
+    }
+
+    function test_UnblockAccount_Admin() public {
+        _unblockAccount(admin);
+    }
+
+    function test_UnblockAccount_Mod() public {
+        _unblockAccount(mod);
+    }
+
+    function _unblockAccount(address sender) internal {
+        vm.prank(sender);
+        joinNFT.setBlockList(member, true);
+
+        assert(joinNFT.isBlock(member));
+        assert(ospClient.isCommunityBlock(TEST_COMMUNITY_ID, member));
+        vm.expectEmit(address(ospClient));
+        emit OspEvents.JoinNFTAccountBlocked(
+            TEST_COMMUNITY_ID,
+            sender,
+            member,
+            false,
+            block.timestamp
+        );
+        vm.prank(sender);
+        assert(joinNFT.setBlockList(member, false));
+        assertFalse(joinNFT.isBlock(member));
+        assertFalse(ospClient.isCommunityBlock(TEST_COMMUNITY_ID, member));
+    }
+
+    function testRevert_unblockAccount_NotOwnerOrAdminOrMod() public {
+        vm.prank(owner);
+        joinNFT.setBlockList(member, true);
         vm.expectRevert(OspErrors.JoinNFTUnauthorizedAccount.selector);
         vm.prank(member);
-        joinNFT.setBlockList(member, true);
-    }
-
-    function test_JoinNFTTransfer() public {
-        address to = address(0x123);
-        uint256 tokenId = joinNFT.tokenOfOwnerByIndex(member, 0);
-        vm.expectEmit(address(ospClient));
-        emit OspEvents.JoinNFTTransferred(TEST_COMMUNITY_ID, tokenId, member, to, block.timestamp);
-        vm.prank(member);
-        joinNFT.transferFrom(member, to, tokenId);
-        assert(joinNFT.ownerOf(tokenId) == to);
-        assert(ospClient.isJoin(TEST_COMMUNITY_ID, to));
-        assertFalse(ospClient.isJoin(TEST_COMMUNITY_ID, member));
-        assertEq(joinNFT.tokenOfOwnerByIndex(to, 0), tokenId);
-    }
-
-    function testRevert_JoinNFTTransfer_ToAddressHasNFT() public {
-        address to = mod;
-        uint256 tokenId = joinNFT.tokenOfOwnerByIndex(member, 0);
-        vm.expectRevert(OspErrors.JoinNFTDuplicated.selector);
-        vm.prank(member);
-        joinNFT.transferFrom(member, to, tokenId);
+        joinNFT.setBlockList(member, false);
     }
 
     function testRevert_JoinNFTTransfer_FromAddressBlocked() public {
@@ -249,5 +383,49 @@ contract JoinNFTRoleTest is JoinNFTTestSetUp {
         vm.expectRevert(OspErrors.JoinNFTBlocked.selector);
         joinNFT.balanceOf(member);
         assertFalse(ospClient.isJoin(TEST_COMMUNITY_ID, member));
+    }
+
+    //transfer
+    function test_JoinNFTTransfer() public {
+        address to = address(0x123);
+        uint256 tokenId = joinNFT.tokenOfOwnerByIndex(member, 0);
+        vm.expectEmit(address(ospClient));
+        emit OspEvents.JoinNFTTransferred(TEST_COMMUNITY_ID, tokenId, member, to, block.timestamp);
+        vm.prank(member);
+        joinNFT.transferFrom(member, to, tokenId);
+        assert(joinNFT.ownerOf(tokenId) == to);
+        assert(ospClient.isJoin(TEST_COMMUNITY_ID, to));
+        assertFalse(ospClient.isJoin(TEST_COMMUNITY_ID, member));
+        assertEq(joinNFT.tokenOfOwnerByIndex(to, 0), tokenId);
+    }
+
+    function testRevert_JoinNFTTransfer_ToAddressHasNFT() public {
+        address to = mod;
+        uint256 tokenId = joinNFT.tokenOfOwnerByIndex(member, 0);
+        vm.expectRevert(OspErrors.JoinNFTDuplicated.selector);
+        vm.prank(member);
+        joinNFT.transferFrom(member, to, tokenId);
+    }
+
+    function test_JoinNFTTransfer_RevokeRole() public {
+        address to = address(0x123);
+        uint256 tokenId = joinNFT.tokenOfOwnerByIndex(admin, 0);
+        vm.expectEmit(address(ospClient));
+        emit OspEvents.JoinNFTRoleChanged(
+            TEST_COMMUNITY_ID,
+            admin,
+            admin,
+            Constants.COMMUNITY_ADMIN_ACCESS,
+            false,
+            block.timestamp
+        );
+        vm.expectEmit(address(ospClient));
+        emit OspEvents.JoinNFTTransferred(TEST_COMMUNITY_ID, tokenId, admin, to, block.timestamp);
+        vm.prank(admin);
+        joinNFT.transferFrom(admin, to, tokenId);
+        assert(joinNFT.ownerOf(tokenId) == to);
+        assert(ospClient.isJoin(TEST_COMMUNITY_ID, to));
+        assertFalse(ospClient.isJoin(TEST_COMMUNITY_ID, admin));
+        assertEq(joinNFT.tokenOfOwnerByIndex(to, 0), tokenId);
     }
 }
